10435f1965b06d03221696b3b069397c74b06bdb
jhjung <jungjh0908@gmail.com>
2015-04-24 10:21:12 +0900

    first

---
+++ src/main/java/com/refrigerator/App.java
@@ -0,0 +1,13 @@
+package com.refrigerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
